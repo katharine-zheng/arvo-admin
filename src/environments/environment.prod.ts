fb673ab2099d9789398d11cb399195eb40a7ea95
katharine-zheng <katharine-zheng@users.noreply.github.com>
2024-09-13 14:30:09 -0400

    added staging config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,5 +10,3 @@ export const environment = {
   },
   production: true
 };
-
-// {"projectId":"arvo-prod","appId":"1:519979280499:web:23018a9d136a162e38a206","storageBucket":"arvo-prod.appspot.com","locationId":"us-central","apiKey":"AIzaSyCJmXxv0Dp8N7nqqhmWq2rqgXxdqNC7WnI","authDomain":"arvo-prod.firebaseapp.com","messagingSenderId":"519979280499","measurementId":"G-4D3CZ5JS16"}
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyD9gOClrVSYeKgyO_9ENhtAMNtRZeLO7zc",
+    authDomain: "arvo-staging.firebaseapp.com",
+    projectId: "arvo-staging",
+    storageBucket: "arvo-staging.appspot.com",
+    messagingSenderId: "1057423515407",
+    appId: "1:1057423515407:web:25fb6ca0a85b181e92287a",
+    measurementId: "G-PDYFTFX790"
+  },
+  production: false
+}
